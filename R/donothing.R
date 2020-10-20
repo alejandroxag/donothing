6bf870b8de1eed30e09eacff851876a719bf5d95
alejandroxag <alejandro.alvarez.1804@gmail.com>
2020-10-20 17:07:28 -0400

    initial commit

---
+++ R/donothing.R
@@ -0,0 +1,24 @@
+#' Do Nothing
+#'
+#' That's that.
+#'
+#' @param arg
+#' Anything.
+#'
+#' @return
+#' arg.
+#'
+#' @export
+#'
+#' @examples
+#'
+#' tibble::tibble(x = 1:3) %>%
+#' dplyr::mutate(y = letters[1:3]) %>%
+#' # dplyr::mutate(y = LETTERS[1:3]) %>%
+#' donothing()
+#'
+#' tibble::tibble(x = 1:3) %>%
+#' # dplyr::mutate(y = letters[1:3]) %>%
+#' dplyr::mutate(y = LETTERS[1:3]) %>%
+#' donothing()
+donothing <- function(arg) return(arg)
